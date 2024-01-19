6a6839c3633f135bf150f764150dfa5bd32604c0
debadatta padhan <psamantaray77@gmail.com>
2024-01-19 19:38:48 +0530

    changes

--- keys/development.keys.js
+++ keys/development.keys.js
@@ -2,6 +2,6 @@ module.exports = {
     'PORT': process.env.PORT || 8001,
     'NODE_ENV': 'development',
     'JWT_SECRET': 'helloLovoso',
-    'MONGODB_URI': 'mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/smartIntern',
+    'MONGODB_URI': 'mongodb+srv://root:livoso@909@cluster0.kllwzgp.mongodb.net/smartIntern',
     'BASEURL': 'http://localhost:8001',
 }
